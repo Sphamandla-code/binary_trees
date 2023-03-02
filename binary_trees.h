c05b0cf7b96768566aca8313d7085424be676fd5
Sphamandla-code <nkambulesphamandla@gmail.com>
2023-03-02 16:14:00 +0200

    binary trees

--- binary_trees.h
+++ binary_trees.h
@@ -82,5 +82,7 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
-
+void bal(avl_t **tree);
+int successor(bst_t *node);
+int remove_type(bst_t *root);
 #endif /* BINARY_TREES_H */
